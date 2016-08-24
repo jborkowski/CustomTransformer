e32fac357ade15f76034ac2b01565644b2d9fa41
Jonatan Borkowski <jonatanborkowski@wp.pl>
2016-08-24 20:37:25 +0200

    It's working!!!! :)

--- src/main/scala/CustomTransformer.scala
+++ src/main/scala/CustomTransformer.scala
@@ -1,9 +1,22 @@
+package custom.transformer
 /**
   * Created by jborkowski on 24/08/16.
   */
 import org.apache.spark.ml._
 import org.apache.spark.ml.util._
+import org.apache.spark.ml.param._
+import org.apache.spark.sql.types.{ArrayType, DataType, StringType}
 
-class CustomTransformer extends UnaryTransformer[String, Seq[String], CustomTransformer] with DefaultParamsWritable {
+class CustomTransformer (override val uid: String)
+extends UnaryTransformer[String, Seq[String], CustomTransformer] with DefaultParamsWritable {
+  def this() = this(Identifiable.randomUID("cto"))
 
+  override protected def createTransformFunc: (String) => Seq[String] = { originStr =>
+//    val re = $(pattern).r
+//    val str = if ($(toUppercase)) originStr.toUpperCase else originStr
+//    val tokens = if ($(gaps)) re.split(str).toSeq else re.findAllIn(str).toSeq
+    originStr.toUpperCase().split(" ")
+  }
+
+  override protected def outputDataType: DataType = new ArrayType(StringType, true)
 }
--- src/test/scala/CustomTransformerSpec.scala
+++ src/test/scala/CustomTransformerSpec.scala
@@ -1,14 +1,24 @@
-package custom.tranformer
+package custom.transformer
 
 import org.scalatest.{FlatSpec, Matchers}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
+
 
 class CustomTransformerSpec extends FlatSpec with Matchers {
   "A Stack" should "pop values in last-in-first-out order" in {
-    //Fake content...
-    val spark = SparkSession.builder().master("local[*]").appName("MyLovelyApp").getOrCreate()
-    val t = new CustomTransformer
-    println(t.explainParams)
+
+    val ss = SparkSession.builder.master("local[*]").getOrCreate
+
+    val sentenceDataFrame = ss.createDataFrame(Seq(
+      (0, "Hi I heard about Spark"),
+      (1, "I wish Java could use case classes"),
+      (2, "Logistic,regression,models,are,neat")
+    )).toDF("label", "sentence")
+    val customTokenizer = (new CustomTransformer).setInputCol("sentence").setOutputCol("words")
+
+    val tokenized = customTokenizer.transform(sentenceDataFrame)
+    tokenized.select("words", "label").show
 
   }
 }
